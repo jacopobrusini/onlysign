3bb1d1f743f81e12dec0c8efdae579a2abb0faa3
Jacopo <fastaugust@icloud.com>
2026-09-17 21:47:29 +0200

    certificate cron

--- app/api/cron/certificate/route.ts
+++ app/api/cron/certificate/route.ts
@@ -132,7 +132,6 @@ await tx.orm.public.PpqcheckTransaction.create({
   description:
     `PPQCheck certificate ${certificate.code ?? order.ppqcheckOrderId}`,
 });
-//
     await tx.orm.public.TokenTransaction.create({
       userId: order.userId,
       amount: -order.tokens,
